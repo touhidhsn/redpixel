f1a5f5ea219107e19de59b9058783b1a0895ff1d
Md Touhid Hasan <touhid@Touhids-MacBook.local>
2022-11-26 15:04:36 +0600

    half of home page developed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customred: "#FB0707",
+        customblack: "#263238",
+        custombackground: "#F4F4F4",
+      },
+    },
+  },
+  plugins: [],
+};
